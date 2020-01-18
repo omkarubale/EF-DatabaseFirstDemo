c997019b1ae4bca689b60582b36d341d70100b97
Omkar Ubale <oubale@gmail.com>
2020-01-18 20:48:57 +0530

    initial commit

---
+++ DatabaseFirstDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new DatabaseFirstDemoEntities();
+            var post = new Post()
+            {
+                Body = "Body2",
+                DatePublished = DateTime.Now,
+                Title = "Title2",
+                PostID = 2
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
+        }
+    }
+}
